2bf2dbe6d325c5209ed4d6fe18fa5824e3dd3ee9
Franjjr <franjjr@gmail.com>
2023-12-18 23:05:15 +0000

    Feat: mo me ha dado tiempo a tarminarlo, me hubier gustado hacer que tambien cambie de color dependiendo del palo

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let palo = ["♦", "♥", "♠", "♣"];
+let paloRandom = palo[Math.floor(Math.random() * palo.length)];
+let numero = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+let numeroRandom = numero[Math.floor(Math.random() * numero.length)];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let color = "rojo";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+if (paloRandom == "♦" || paloRandom == "♥") {
+  color = "rojo";
+} else {
+  color = "negro";
+}
+
+function generar() {
+  document.querySelector(".icono").innerHTML = paloRandom;
+  document.querySelector(".icono2").innerHTML = paloRandom;
+  document.querySelector(".carta").innerHTML = numeroRandom;
+}
